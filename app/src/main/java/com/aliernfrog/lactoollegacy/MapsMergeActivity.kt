fe825b24c75c4fb1f5bb18fe5aecc68fb365d0cb
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2023-03-30 19:01:05 +0300

    feat(map-merge): merge maps with laclib

--- app/src/main/java/com/aliernfrog/lactoollegacy/MapsMergeActivity.kt
+++ app/src/main/java/com/aliernfrog/lactoollegacy/MapsMergeActivity.kt
@@ -1,264 +1,196 @@
-package com.aliernfrog.lactoollegacy;
+package com.aliernfrog.lactoollegacy
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
+import android.content.Intent
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.view.View
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import com.aliernfrog.laclib.data.LACMapToMerge
+import com.aliernfrog.laclib.map.LACMapMerger
+import com.aliernfrog.lactoollegacy.fragments.MapDownloadSheet.MapDownloadListener
+import com.aliernfrog.lactoollegacy.fragments.MapPickerSheet
+import com.aliernfrog.lactoollegacy.fragments.MapPickerSheet.MapPickerListener
+import com.aliernfrog.lactoollegacy.utils.AppUtil
+import com.aliernfrog.lactoollegacy.utils.FileUtil
+import com.google.android.material.textfield.TextInputEditText
+import java.io.File
 
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.os.Bundle;
-import android.text.TextUtils;
-import android.view.View;
-import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-import android.widget.Toast;
+@Suppress("DEPRECATION")
+class MapsMergeActivity : AppCompatActivity(), MapPickerListener, MapDownloadListener {
+    private lateinit var toolbar: Toolbar
+    private lateinit var baseMapLinear: LinearLayout
+    private lateinit var baseMapName: TextView
+    private lateinit var mapToAddLinear: LinearLayout
+    private lateinit var mapToAddName: TextView
+    private lateinit var mapToAddIncludeSpawns: CheckBox
+    private lateinit var mapToAddIncludeTdmSpawns: CheckBox
+    private lateinit var mapToAddIncludeCheckpoints: CheckBox
+    private lateinit var mapToAddPos: TextInputEditText
+    private lateinit var outputLinear: LinearLayout
+    private lateinit var outputMapName: TextInputEditText
+    private lateinit var startMerge: Button
+    private lateinit var debugText: TextView
+    private lateinit var prefsConfig: SharedPreferences
 
-import com.aliernfrog.lactoollegacy.fragments.MapDownloadSheet;
-import com.aliernfrog.lactoollegacy.fragments.MapPickerSheet;
-import com.aliernfrog.lactoollegacy.utils.AppUtil;
-import com.aliernfrog.lactoollegacy.utils.FileUtil;
-import com.google.android.material.textfield.TextInputEditText;
+    private var mapsPath: String? = null
+    private var primaryMapPath: String? = ""
+    private var secondaryMapPath: String? = ""
+    private var currentPickRequest: String? = null
 
-import java.io.File;
-import java.util.ArrayList;
-import java.util.Arrays;
+    private val primaryMap = "primary"
+    private val secondaryMap = "secondary"
+    private val pickMapRequest = 1
 
-public class MapsMergeActivity extends AppCompatActivity implements MapPickerSheet.MapPickerListener, MapDownloadSheet.MapDownloadListener {
-    Toolbar toolbar;
-    LinearLayout baseMapLinear;
-    TextView baseMapName;
-    LinearLayout mapToAddLinear;
-    TextView mapToAddName;
-    CheckBox mapToAddIncludeSpawns;
-    CheckBox mapToAddIncludeTdmSpawns;
-    CheckBox mapToAddIncludeCheckpoints;
-    TextInputEditText mapToAddPos;
-    LinearLayout outputLinear;
-    TextInputEditText outputMapName;
-    Button startMerge;
-    TextView debugText;
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_maps_merge)
+        mapsPath = intent.getStringExtra("mapsPath")
 
-    SharedPreferences prefsConfig;
+        toolbar = findViewById(R.id.mapsMerge_toolbar)
+        baseMapLinear = findViewById(R.id.mapsMerge_baseMap_linear)
+        baseMapName = findViewById(R.id.mapsMerge_baseMap_name)
+        mapToAddLinear = findViewById(R.id.mapsMerge_mapToAdd_linear)
+        mapToAddName = findViewById(R.id.mapsMerge_mapToAdd_name)
+        mapToAddIncludeSpawns = findViewById(R.id.mapsMerge_mapToAdd_includeSpawns)
+        mapToAddIncludeTdmSpawns = findViewById(R.id.mapsMerge_mapToAdd_includeTdmSpawns)
+        mapToAddIncludeCheckpoints = findViewById(R.id.mapsMerge_mapToAdd_includeCheckpoints)
+        mapToAddPos = findViewById(R.id.mapsMerge_mapToAdd_pos)
+        outputLinear = findViewById(R.id.mapsMerge_output_linear)
+        outputMapName = findViewById(R.id.mapsMerge_output_name)
+        startMerge = findViewById(R.id.mapsMerge_output_merge)
+        debugText = findViewById(R.id.mapsMerge_debug)
 
-    String mapsPath;
-    String primaryMapPath = "";
-    String secondaryMapPath = "";
-    String currentPickRequest;
+        prefsConfig = getSharedPreferences("APP_CONFIG", MODE_PRIVATE)
+        if (prefsConfig.getBoolean("enableDebug", false)) debugText.visibility = View.VISIBLE
 
-    final String PRIMARY_MAP = "primary";
-    final String SECONDARY_MAP = "secondary";
-    final int REQUEST_PICK_MAP = 1;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps_merge);
-
-        mapsPath = getIntent().getStringExtra("mapsPath");
-
-        toolbar = findViewById(R.id.mapsMerge_toolbar);
-        baseMapLinear = findViewById(R.id.mapsMerge_baseMap_linear);
-        baseMapName = findViewById(R.id.mapsMerge_baseMap_name);
-        mapToAddLinear = findViewById(R.id.mapsMerge_mapToAdd_linear);
-        mapToAddName = findViewById(R.id.mapsMerge_mapToAdd_name);
-        mapToAddIncludeSpawns = findViewById(R.id.mapsMerge_mapToAdd_includeSpawns);
-        mapToAddIncludeTdmSpawns = findViewById(R.id.mapsMerge_mapToAdd_includeTdmSpawns);
-        mapToAddIncludeCheckpoints = findViewById(R.id.mapsMerge_mapToAdd_includeCheckpoints);
-        mapToAddPos = findViewById(R.id.mapsMerge_mapToAdd_pos);
-        outputLinear = findViewById(R.id.mapsMerge_output_linear);
-        outputMapName = findViewById(R.id.mapsMerge_output_name);
-        startMerge = findViewById(R.id.mapsMerge_output_merge);
-        debugText = findViewById(R.id.mapsMerge_debug);
-
-        prefsConfig = getSharedPreferences("APP_CONFIG", MODE_PRIVATE);
-        if (prefsConfig.getBoolean("enableDebug", false)) debugText.setVisibility(View.VISIBLE);
-
-        devLog("MapsMergeActivity started");
-
-        setListeners();
+        devLog("MapsMergeActivity started")
+        setListeners()
     }
 
-    public void checkStuffAndStartMerging() {
-        if (primaryMapPath.equals("") || secondaryMapPath.equals("")) {
-            Toast.makeText(getApplicationContext(), R.string.mapMerge_warning_noMapSelected, Toast.LENGTH_SHORT).show();
-        } else if (getMapToAddPosition() == null) {
-            Toast.makeText(getApplicationContext(), R.string.mapMerge_warning_posNotValid, Toast.LENGTH_SHORT).show();
-        } else if (getOutputName() == null) {
-            Toast.makeText(getApplicationContext(), R.string.mapMerge_warning_noOutputName, Toast.LENGTH_SHORT).show();
-        } else {
-            File check = new File(mapsPath+"/"+getOutputName()+".txt");
-            devLog("checking path: "+check.getPath());
-            if (check.exists()) {
-                Toast.makeText(getApplicationContext(), R.string.denied_alreadyExists, Toast.LENGTH_SHORT).show();
-            } else {
-                devLog("passed check, map to add pos: "+ TextUtils.join(",", getMapToAddPosition()));
-                mergeMap();
-            }
-        }
+    private fun validateAndMerge() {
+        if (primaryMapPath == "" || secondaryMapPath == "")
+            return Toast.makeText(applicationContext, R.string.mapMerge_warning_noMapSelected, Toast.LENGTH_SHORT).show()
+        if (getOutputName() == null)
+            return Toast.makeText(applicationContext, R.string.mapMerge_warning_noOutputName, Toast.LENGTH_SHORT).show()
+        val check = File("$mapsPath/${getOutputName()}.txt")
+        devLog("checking path: " + check.path)
+        if (check.exists())
+            return Toast.makeText(applicationContext, R.string.denied_alreadyExists, Toast.LENGTH_SHORT).show()
+        devLog("passed checks, merging")
+        mergeMap()
     }
 
-    public void mergeMap() {
+    private fun mergeMap() {
         try {
-            String primaryMapRaw = FileUtil.readFile(primaryMapPath);
-            String secondaryMapRaw = FileUtil.readFile(secondaryMapPath);
-            ArrayList<String> primaryMapLines = new ArrayList<>(Arrays.asList(primaryMapRaw.split("\n")));
-            ArrayList<String> secondaryMapLines = new ArrayList<>(Arrays.asList(secondaryMapRaw.split("\n")));
-            for (int i = 0; i < secondaryMapLines.size(); i++) {
-                String line = secondaryMapLines.get(i);
-                if (line.split(":").length > 1 && line.split(":")[0].contains("_Editor")) {
-                    //editor object
-                    if (checkObjectForMerge(line)) primaryMapLines.add(getNewObjectPos(line));
-                } else if (line.startsWith("Vehicle_")) {
-                    //vehicle
-                    primaryMapLines.add(getNewVehiclePos(line));
-                } else if (line.startsWith("Downloadable_Content_Material")) {
-                    //downloadable material
-                    primaryMapLines.add(line);
-                }
-            }
-            saveMergedMap(TextUtils.join("\n", primaryMapLines));
-        } catch (Exception e) {
-            e.printStackTrace();
-            devLog(e.toString());
+            val primaryMapRaw = FileUtil.readFile(primaryMapPath)
+            val secondaryMapRaw = FileUtil.readFile(secondaryMapPath)
+            val mapMerger = LACMapMerger(mutableListOf(
+                LACMapToMerge(
+                    mapName = primaryMapPath!!,
+                    content = primaryMapRaw,
+                    mergeSpawnpoints = true,
+                    mergeRacingCheckpoints = true,
+                    mergeTDMSpawnpoints = true
+                ),
+                LACMapToMerge(
+                    mapName = secondaryMapPath!!,
+                    content = secondaryMapRaw,
+                    mergeSpawnpoints = mapToAddIncludeSpawns.isChecked,
+                    mergeRacingCheckpoints = mapToAddIncludeCheckpoints.isChecked,
+                    mergeTDMSpawnpoints = mapToAddIncludeTdmSpawns.isChecked,
+                    mergePosition = mapToAddPos.text.toString()
+                )
+            ))
+            val mergedContent = mapMerger.mergeMaps(
+                onNoEnoughMaps = { /* Imposible */ }
+            )!!
+            saveMergedMap(mergedContent)
+        } catch (e: Exception) {
+            e.printStackTrace()
+            devLog(e.toString())
         }
     }
 
-    public void saveMergedMap(String content) {
+    private fun saveMergedMap(content: String) {
         try {
-            String fileName = getOutputName() + ".txt";
-            devLog("attempting to save merged map with name: " + fileName);
-            FileUtil.saveFile(mapsPath, fileName, content);
-            Toast.makeText(getApplicationContext(), R.string.info_done, Toast.LENGTH_SHORT).show();
-        } catch (Exception e) {
-            e.printStackTrace();
-            devLog(e.toString());
+            val fileName = "${getOutputName()}.txt"
+            devLog("attempting to save merged map with name: $fileName")
+            FileUtil.saveFile(mapsPath, fileName, content)
+            Toast.makeText(applicationContext, R.string.info_done, Toast.LENGTH_SHORT).show()
+        } catch (e: Exception) {
+            e.printStackTrace()
+            devLog(e.toString())
         }
     }
 
-    public void getMap(String path) {
-        devLog(currentPickRequest+": setting path: "+path);
-        File file = new File(path);
-        String name = FileUtil.removeExtension(file.getName());
-        if (currentPickRequest.equals(PRIMARY_MAP)) {
-            primaryMapPath = path;
-            baseMapName.setText(name);
-        } else if (currentPickRequest.equals(SECONDARY_MAP)) {
-            secondaryMapPath = path;
-            mapToAddName.setText(name);
+    private fun getMap(path: String) {
+        devLog("$currentPickRequest: setting path: $path")
+        val file = File(path)
+        val name = FileUtil.removeExtension(file.name)
+        if (currentPickRequest == primaryMap) {
+            primaryMapPath = path
+            baseMapName.text = name
+        } else if (currentPickRequest == secondaryMap) {
+            secondaryMapPath = path
+            mapToAddName.text = name
         }
     }
 
-    void pickMap(String request) {
-        currentPickRequest = request;
-        Bundle bundle = new Bundle();
-        bundle.putString("mapsPath", mapsPath);
-        MapPickerSheet mapPickerSheet = new MapPickerSheet();
-        mapPickerSheet.setArguments(bundle);
-        mapPickerSheet.show(getSupportFragmentManager(), "map_pick");
+    private fun pickMap(request: String) {
+        currentPickRequest = request
+        val bundle = Bundle()
+        bundle.putString("mapsPath", mapsPath)
+        val mapPickerSheet = MapPickerSheet()
+        mapPickerSheet.arguments = bundle
+        mapPickerSheet.show(supportFragmentManager, "map_pick")
     }
 
-    Boolean checkObjectForMerge(String line) {
-        String object = line.split(":")[0];
-        if (object.equals("Spawn_Point_Editor")) return mapToAddIncludeSpawns.isChecked();
-        if (object.startsWith("Team_")) return mapToAddIncludeTdmSpawns.isChecked();
-        if (object.startsWith("Checkpoint_Editor")) return mapToAddIncludeCheckpoints.isChecked();
-        return true;
+    private fun getOutputName(): String? {
+        if (outputMapName.text == null) return null
+        val name = outputMapName.text.toString()
+        return if (name == "") null else name
     }
 
-    String getNewObjectPos(String line) {
-        String[] lineSplit = line.split(":");
-        String posStr = lineSplit[1];
-        String newPosStr = getNewPos(posStr, ",");
-        lineSplit[1] = newPosStr;
-        return TextUtils.join(":", lineSplit);
+    private fun devLog(toLog: String) {
+        AppUtil.devLog(toLog, debugText)
     }
 
-    String getNewVehiclePos(String line) {
-        String[] lineSplit = line.split(":");
-        String posStr = lineSplit[1];
-        String newPosStr = getNewPos(posStr, ", ");
-        lineSplit[1] = newPosStr;
-        return TextUtils.join(":", lineSplit);
-    }
-
-    String getNewPos(String str, String joinWith) {
-        String[] posSplit = str.split(joinWith);
-        String[] posToAddSplit = getMapToAddPosition();
-        Double posX = Double.parseDouble(posSplit[0]);
-        Double posY = Double.parseDouble(posSplit[1]);
-        Double posZ = Double.parseDouble(posSplit[2]);
-        Double posToAddX = Double.parseDouble(posToAddSplit[0]);
-        Double posToAddY = Double.parseDouble(posToAddSplit[1]);
-        Double posToAddZ = Double.parseDouble(posToAddSplit[2]);
-        double newX = posX+posToAddX;
-        double newY = posY+posToAddY;
-        double newZ = posZ+posToAddZ;
-        return newX+joinWith+newY+joinWith+newZ;
-    }
-
-    String getOutputName() {
-        if (outputMapName.getText() == null) return null;
-        String name = outputMapName.getText().toString();
-        if (name.equals("")) return null;
-        return name;
-    }
-
-    String[] getMapToAddPosition() {
-        if (mapToAddPos.getText() == null) return null;
-        String rawPos = mapToAddPos.getText().toString();
-        String[] split = rawPos.split(",");
-        if (split.length != 3) return null;
-        boolean isNumber = true;
-        for (String str : split) {
-            Boolean strIsNumber = AppUtil.stringIsNumber(str);
-            if (!strIsNumber) isNumber = false;
-        }
-        if (!isNumber) return null;
-        return split;
-    }
-
-    void devLog(String toLog) {
-        AppUtil.devLog(toLog, debugText);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        boolean hasData = data != null;
-        devLog(requestCode+": hasData = "+hasData);
-        if (!hasData) return;
-        if (requestCode == REQUEST_PICK_MAP) {
-            String path = data.getStringExtra("path");
-            devLog("received path: "+path);
-            getMap(path);
+    @Deprecated("Deprecated in Java")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        val hasData = data != null
+        devLog("$requestCode: hasData = $hasData")
+        if (!hasData) return
+        if (requestCode == pickMapRequest) {
+            val path = data!!.getStringExtra("path")
+            devLog("received path: $path")
+            getMap(path!!)
         }
     }
 
-    void setListeners() {
-        toolbar.setNavigationOnClickListener(v -> finish());
-        AppUtil.handleOnPressEvent(baseMapLinear, () -> pickMap(PRIMARY_MAP));
-        AppUtil.handleOnPressEvent(mapToAddLinear, () -> pickMap(SECONDARY_MAP));
-        AppUtil.handleOnPressEvent(outputLinear);
-        AppUtil.handleOnPressEvent(startMerge, this::checkStuffAndStartMerging);
+    fun setListeners() {
+        toolbar.setNavigationOnClickListener { finish() }
+        AppUtil.handleOnPressEvent(baseMapLinear) { pickMap(primaryMap) }
+        AppUtil.handleOnPressEvent(mapToAddLinear) { pickMap(secondaryMap) }
+        AppUtil.handleOnPressEvent(outputLinear)
+        AppUtil.handleOnPressEvent(startMerge) { validateAndMerge() }
     }
 
-    @Override
-    public void onMapPicked(String path) {
-        getMap(path);
+    override fun onMapPicked(path: String) {
+        getMap(path)
     }
 
-    @Override
-    public void onFilePickRequested() {
-        devLog("attempting to pick a map file");
-        Intent intent = new Intent(this, FilePickerActivity.class);
-        intent.putExtra("FILE_TYPE_SAF", "text/*");
-        intent.putExtra("FILE_TYPE_INAPP", new String[]{"txt"});
-        startActivityForResult(intent, REQUEST_PICK_MAP);
+    override fun onFilePickRequested() {
+        devLog("attempting to pick a map file")
+        val intent = Intent(this, FilePickerActivity::class.java)
+        intent.putExtra("FILE_TYPE_SAF", "text/*")
+        intent.putExtra("FILE_TYPE_INAPP", arrayOf("txt"))
+        startActivityForResult(intent, pickMapRequest)
     }
 
-    @Override
-    public void onMapDownloaded(String path) {
-        getMap(path);
+    override fun onMapDownloaded(path: String) {
+        getMap(path)
     }
 }
