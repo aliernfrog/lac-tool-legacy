3b8c8314649ad09859ebc01278f351c61ebdd94b
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2024-12-22 18:34:58 +0300

    chore: enable multidex

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,6 +13,7 @@ android {
         targetSdk = 31
         versionCode = 22000
         versionName = "2.2.0"
+        multiDexEnabled = true
     }
 
     buildTypes {
@@ -41,5 +42,6 @@ dependencies {
     implementation("androidx.coordinatorlayout:coordinatorlayout:1.2.0")
     implementation("com.github.aliernfrog:laclib:1.2.0")
     implementation("com.github.HBiSoft:PickiT:2.0.5")
+    implementation("com.android.support:multidex:2.0.1")
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.3")
 }
